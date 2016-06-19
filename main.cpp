12984f3d795a8a42ab2716d5f17bff50bc47d31c
agentflare <samuel.wu.u1@gmail.com>
2016-06-19 13:49:59 -0400

    Small fix to seeding

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 
 int main(int args, char* argv[]) {
     if(args>1){
-        int seed = atoi(argv[0]);
+        int seed = atoi(argv[1]);
         Game new_game = Game(seed);
     }
     else{
-        Game new_game = Game();
+        Game new_game = Game(0);
     }
     exit(0);
 }
